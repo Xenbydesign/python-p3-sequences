99c3adf1fb6a531bda814c6bbe3731155e211a17
Xennia Contreras <Xenbydesign@gmail.com>
2024-03-13 19:26:45 -0700

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci_seq = [0, 1] if length > 1 else [0] if length == 1 else []
+    while len(fibonacci_seq) < length:
+        fibonacci_seq.append(fibonacci_seq[-1] + fibonacci_seq[-2])
+    print(fibonacci_seq)
